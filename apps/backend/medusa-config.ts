d781d5d3a5ce910dc9dc4f2a0d22ddae25eac4c6
vignesh-lk <vignesh.s@liviktech.com>
2026-06-08 10:51:12 +0530

    Update medusa-config.ts

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -5,6 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redis_url: REDIS_URL,
+    database_type: "postgres",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
